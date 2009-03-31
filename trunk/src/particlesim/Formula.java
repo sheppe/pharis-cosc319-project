3c8b0ced27b0195bec961e478095a0732db6a998
zenith916 <zenith916@473bfc78-e73c-11dd-96f3-21a44f33db77>
2009-03-31 08:09:42 +0000

    Fixed bugs and add java doc comments in this class.

--- trunk/src/particlesim/Formula.java
+++ trunk/src/particlesim/Formula.java
@@ -1,38 +1,59 @@
 package particlesim;
 
+import java.util.Random;
+
 /**
  * @author Yuehan
  */
 public class Formula {
 
-    private Matrix tensor_0,  rotation,  rotation_invers,  field,  tensor;
-    /* Generate a random number for initial orientation of particle (in lab frame),
-     * then convert it as degree.
-     */
-    private double degree = (Math.random()) / 180 * Math.PI;
-    private double differDeg, degByTime ;
+   private Matrix tensor_0, rotation, rotation_invers, field;
 
-    /**
-     * @param rotaMobility rotational mobility
-     * @param PAPParalle the polarizability alone principle axis parallely
-     * @param PAPVertical the polarizability alone principle axis vertically
-     * @param fieldNull
-     * @param omega
-     */
-    public Formula(double rotaMobility, double PAPParalle, double PAPVertical, double fieldNull, double omega) {
+   // Generate a random number for initial orientation of particle
+   //(in lab frame), then convert it as degree.
+   // range
+   private double degree;
 
-        // formula for changing angle by one time step.
-        differDeg = (-fieldNull * (PAPParalle - PAPVertical) * Math.sin(2 * (degree - omega)) * 1) / 180 * Math.PI;
+   //it holds the vlaue of polarizability along
+   //principle axis parallely
+   private double PAPParalle;
 
-        // ccurrent angle after one time step.
-        degByTime = degree + differDeg;
+   //it holds the vlaue of polarizability along
+   //principle axis vertically
+   private double PAPVertical;
 
+   //field Null
+   private double fieldNull;
+
+   //omega initial to 0 at this moment
+   private double omega = 0;
+
+   //holds the value of rotational mobility
+   private double rotaMobility;
+
+   //the degree by time is a variable needed to constructed
+   //a matrix, the differenct degree calculats the degByTime
+   private double differDeg, degByTime;
+
+   private final int deltaT = 1;
+
+   public Formula(double PAPParalle, double PAPVertical, double fieldNull, double omega, double rotaMobility) {
+
+       this.setPAPParalle(PAPParalle);
+       this.setPAPVertical(PAPVertical);
+       this.setFieldNull(fieldNull);
+       this.setOmega(omega);
+       this.setRotaMobility(rotaMobility);
+       this.setIniDegree();
+       this.setDifferDeg();
+       this.setDegByTime();
+       
         // Create rotation matrix R
-        double[][] rota = {{Math.cos(degByTime), Math.sin(degByTime)}, {(-Math.sin(degByTime)), Math.cos(degByTime)}};
+        double[][] rota = {{Math.cos(this.degByTime), Math.sin(this.degByTime)}, {(-Math.sin(this.degByTime)), Math.cos(this.degByTime)}};
         Matrix R = new Matrix(rota);
 
         // Create matrix which is the invers of rotation matrix.
-        double[][] rota_invers = {{Math.cos(degByTime), (-Math.sin(degByTime))}, {Math.sin(degByTime), Math.cos(degByTime)}};
+        double[][] rota_invers = {{Math.cos(this.degByTime), (-Math.sin(this.degByTime))}, {Math.sin(this.degByTime), Math.cos(this.degByTime)}};
         Matrix R_invers = new Matrix(rota_invers);
 
         /*
@@ -41,17 +62,13 @@ public class Formula {
          */
 
         //create matrix for tensor 0 (alpha 0)
-        double[][] a_0 = {{PAPParalle, 0}, {0, PAPVertical}};
+        double[][] a_0 = {{this.PAPParalle, 0}, {0, this.PAPVertical}};
         Matrix tensor_0 = new Matrix(a_0);
 
 
-        //using above 3 matrix to calculate matrix tensor (alpha) (this is formular 2)
-        Matrix tensor = R_invers.multiply(tensor_0).multiply(R);
-
-
-        // create filed matrix E;
-        double E_x = fieldNull * Math.cos(omega);
-        double E_y = fieldNull * Math.sin(omega);
+       // create filed matrix E;
+        double E_x = this.fieldNull * Math.cos(this.omega);
+        double E_y = this.fieldNull * Math.sin(this.omega);
         double[][] field = {{E_x}, {E_y}};
         Matrix E = new Matrix(field);
 
@@ -59,33 +76,108 @@ public class Formula {
         this.rotation = R;
         this.rotation_invers = R_invers;
         this.tensor_0 = tensor_0;
-        this.tensor = tensor;
         this.field = E;
     }
 
+   public double getPAPParalle(){
+       return this.PAPParalle;
+   }
 
-    // Return the rotation angle by one time step
-    public double getDegByTime(){
-        return degByTime;
-    }
+   public double getPAPVertical(){
+       return this.PAPVertical;
+   }
 
+   public double getFieldNull(){
+       return this.fieldNull;
+   }
 
-    // calculate Matrix P (this is formular 1)
-    public Matrix solv_P() {
-        Matrix P = tensor.s_multiply(field);
-        return P;
-    }
+   public double getOmega(){
+       return this.omega;
+   }
 
+   public double getRotaMobility(){
+       return this.rotaMobility;
+   }
 
-    // This is formula 2, calculation part did in the constructor
-    public Matrix solv_tensor() {
-        return tensor;
-    }
+   public double getInitDegree(){
+       return this.degree;
+   }
 
+   public double getDifferDeg(){
+       return this.differDeg;
+   }
+
+   public double getDegByTime(){
+       return this.degByTime;
+   }
+
+   public void setPAPParalle(double PAPParalle){
+      this.PAPParalle = PAPParalle;
+   }
+
+   public void setPAPVertical(double PAPVertical){
+      this.PAPVertical = PAPVertical;
+   }
+
+   public void setFieldNull(double fieldNull){
+      this.fieldNull = fieldNull;
+   }
+
+   public void setOmega(double omega){
+      this.omega = omega;
+   }
+
+   public void setRotaMobility(double rotaMobility){
+      this.rotaMobility = rotaMobility;
+   }
+
+    /**
+    * Generates a random initial degree for the particle
+    * range from 0 to 360
+    */
+   public void setIniDegree(){
+       Random ran = new Random();
+       double ranNumber = ran.nextDouble() * 360;
+       this.degree = ranNumber;
+   }
+
+   /**
+    * calculates the Difference degree by 1 second
+    */
+   public void setDifferDeg(){
+      // formula for changing angle by one time step
+      double oneTimeStepDeg = -this.rotaMobility * this.fieldNull * (this.PAPParalle - this.PAPVertical) * Math.sin(2 * (this.degree - this.omega)) * this.deltaT;
+
+      this.differDeg = oneTimeStepDeg;
+   }
+
+   /**
+    * calculates the current rotation degree of the partical
+    */
+   public void setDegByTime(){
+        // ccurrent angle after one time step.
+       double currentDegree = (this.degree + this.differDeg) % 360;
+
+        this.degByTime = currentDegree;
+   }
+   
     
-    // formula 3, this methods does the same thing as method matrix solv_P does.
-    public Matrix solv_Phard() {
-        Matrix P = rotation_invers.multiply(tensor_0).multiply(rotation).s_multiply(field);
+
+    /**
+     * solve the dipole moment P on partical
+     * @return a matrix of dipolmoment.
+     */
+    public Matrix solv_P() {
+        Matrix P =  rotation_invers.multiply(tensor_0).multiply(rotation).s_multiply(field);
         return P;
+    }
+
+    /**
+     * sovle the tensor
+     * @return a matrix of tensor.
+     */
+    public Matrix solv_tensor() {
+        Matrix tensor = rotation_invers.multiply(tensor_0).multiply(rotation);
+        return tensor;
     }
 }
